f4bc61eb935bd7d8fb1505cc29ef690dd68e549e
jeff.dev <johan.06.92@gmail.com>
2025-01-17 22:13:00 +0100

    Volver al servicio privado para mejorar el codigo

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -10,6 +10,18 @@ import { DbzService } from '../services/dbz.service';
 
 export class MainPageComponent  {
   constructor(
-    public dbzService: DbzService
+    private dbzService: DbzService
   ) {}
+
+  get characters(): Character[] {
+    return [...this.dbzService.characters];
+  }
+
+  onDeleteCharacter(id: string): void {
+    this.dbzService.onDeleteCharacterById(id);
+  }
+
+  onNewCharacter(character: Character): void {
+    this.dbzService.addCharacter(character);
+  }
 }
